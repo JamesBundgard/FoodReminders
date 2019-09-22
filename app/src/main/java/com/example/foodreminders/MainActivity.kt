c9f571b92c6acbd9cf3f55f2ef1692b9d8940352
JamesBundgard <jamesbundgard@gmail.com>
2019-09-22 18:20:57 -0400

    Added notifications to remind the user when food is expiring soon.

--- app/src/main/java/com/example/foodreminders/MainActivity.kt
+++ app/src/main/java/com/example/foodreminders/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.example.foodreminders
 
 import android.annotation.SuppressLint
+import android.app.*
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.time.LocalDate
+import java.io.BufferedReader
+import java.io.FileInputStream
+import java.io.IOException
+import java.io.InputStreamReader
 import java.util.*
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import android.widget.Button
-import java.io.*
 import kotlin.collections.ArrayList
 
 
@@ -119,11 +121,19 @@ class MainActivity : AppCompatActivity() {
         println(mFoodItems[0].toString(""))
 
         for (item in mFoodItems){
+            createNotification(item)
             val foodView: LinearLayout = createFoodView(this, item)
             foodView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT)
             list.addView(foodView)
         }
+    }
+
+    private fun createNotification(item: FoodItem){
+        var time = item.ExpiryDate.time - 2*24*60*60*1000 //sends the notification 2 days before
+        if (time <= System.currentTimeMillis()+1000) time = System.currentTimeMillis()+5000
+        NotificationUtils().setNotification(time, this@MainActivity)
+        return
     }
 
     @SuppressLint("SetTextI18n")
@@ -165,7 +175,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
-
         return true
     }
 
@@ -188,3 +197,125 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val service = Intent(context, NotificationService::class.java)
+        service.putExtra("reason", intent.getStringExtra("reason"))
+        service.putExtra("timestamp", intent.getLongExtra("timestamp", 0))
+
+        context.startService(service)
+    }
+}
+
+class NotificationService : IntentService("NotificationService") {
+    private lateinit var mNotification: Notification
+    private val mNotificationId: Int = 1000
+
+    @SuppressLint("NewApi")
+    private fun createChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val context = this.applicationContext
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val notificationChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance)
+            notificationChannel.enableVibration(true)
+            notificationChannel.setShowBadge(true)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.parseColor("#e8334a")
+            notificationChannel.description = getString(R.string.notification_channel_description)
+            notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "food.reminders.notification.id"
+        const val CHANNEL_NAME = "Food expiry notifications"
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+
+        createChannel()
+
+        var timestamp: Long = 0
+        if (intent != null && intent.extras != null) {
+            timestamp = intent.extras!!.getLong("timestamp")
+        }
+
+        if (timestamp > 0) {
+
+            val context = this.applicationContext
+            val notifyIntent = Intent(this, MainActivity::class.java)
+            val title = "You have food expiring soon!"
+            val message = "Press here to see which items are expiring."
+
+            notifyIntent.putExtra("title", title)
+            notifyIntent.putExtra("message", message)
+            notifyIntent.putExtra("notification", true)
+            notifyIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = timestamp
+
+            val pendingIntent = PendingIntent.getActivity(context, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val res = this.resources
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                mNotification = Notification.Builder(this, CHANNEL_ID)
+                    // Set the intent that will fire when the user taps the notification
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setLargeIcon(BitmapFactory.decodeResource(res, R.mipmap.ic_launcher))
+                    .setAutoCancel(true)
+                    .setContentTitle(title)
+                    .setStyle(Notification.BigTextStyle()
+                        .bigText(message))
+                    .setContentText(message).build()
+            }
+            else
+            {
+                mNotification = Notification.Builder(this)
+                    // Set the intent that will fire when the user taps the notification
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setLargeIcon(BitmapFactory.decodeResource(res, R.mipmap.ic_launcher))
+                    .setAutoCancel(true)
+                    .setPriority(Notification.PRIORITY_MAX)
+                    .setContentTitle(title)
+                    .setStyle(
+                        Notification.BigTextStyle()
+                        .bigText(message))
+                    .setContentText(message).build()
+            }
+
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            // mNotificationId is a unique int for each notification that you must define
+            notificationManager.notify(mNotificationId, mNotification)
+        }
+    }
+}
+
+
+class NotificationUtils {
+
+    fun setNotification(timeInMilliSeconds: Long, activity: Activity) {
+        if (timeInMilliSeconds > 0) {
+            val alarmManager = activity.getSystemService(Activity.ALARM_SERVICE) as AlarmManager
+            val alarmIntent = Intent(activity.applicationContext, AlarmReceiver::class.java) // AlarmReceiver1 = broadcast receiver
+
+            alarmIntent.putExtra("reason", "notification")
+            alarmIntent.putExtra("timestamp", timeInMilliSeconds)
+
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = timeInMilliSeconds
+
+            val pendingIntent = PendingIntent.getBroadcast(activity, 0, alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT)
+            alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+        }
+    }
+}
